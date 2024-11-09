d051fe7b3463c1ed571d44c76ed40ff845435bbd
oumaima ait said <oumaimabellanova@gmail.com>
2024-11-09 18:11:34 +0100

    update the project structure by adding tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./main.js", "./assets/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
