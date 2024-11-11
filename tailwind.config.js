4368f463715652f2009fb5b77025f94a872f455d
oumaima ait said <oumaimabellanova@gmail.com>
2024-11-11 10:00:30 +0100

    Adding picture in product Détails

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./index.html", "./main.js", "./assets/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: '#2B2D42',          // Background Color   #2B2D42
+        titre: '#EDF2F4',         // Light background color #EDF2F4
+        sous: '#C7CAD1',          // Little titles  #C7CAD1
+        red: '#EF233C',           // Button color #EF233C 
+        btnhover: '#D90429',      // Hover Color #D90429
+    },
+    fontFamily:{
+      primary : ['Poppins', 'sans-serif'],
+      logo : ['Permanent Marker', 'cursive'],
+    }
+    },
   },
   plugins: [],
 }
