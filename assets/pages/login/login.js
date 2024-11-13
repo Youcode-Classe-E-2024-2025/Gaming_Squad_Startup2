62cb40b17d70be410e4be146d5af02f071e78e1c
oumaima ait said <oumaimabellanova@gmail.com>
2024-11-13 16:55:13 +0100

    add js for change the position Form

--- assets/pages/login/login.js
+++ assets/pages/login/login.js
@@ -1,2 +1,17 @@
-   
+const signUpLink = document.querySelector('.signUpBtn-link');
+const signInLink = document.querySelector('.signInBtn-link');
+
+if (signUpLink && signInLink) {
+    signUpLink.addEventListener('click', (e) => {
+        e.preventDefault();
+        document.querySelector('.sign-in').style.transform = 'translateY(-100%)';
+        document.querySelector('.sign-up').style.transform = 'translateY(0)';
+    });
+
+    signInLink.addEventListener('click', (e) => {
+        e.preventDefault();
+        document.querySelector('.sign-in').style.transform = 'translateY(0)';
+        document.querySelector('.sign-up').style.transform = 'translateY(100%)';
+    });
+}
        
