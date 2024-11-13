1cf8e25435f4d3d444c7609ba1141b1235394b03
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2024-11-13 15:42:19 +0100

    add openForm() and closeForm() functionalities

--- assets/pages/products/products.js
+++ assets/pages/products/products.js
@@ -28,3 +28,18 @@ function displayProducts(products) {
 	const productsAsHtml = products.map((product) => ProductCard(product));
 	productsWrapper.innerHTML = productsAsHtml.join("");
 }
+
+const addProductBtn = document.querySelector(".add-product-btn");
+addProductBtn.onclick = () => openForm();
+
+function openForm(whichForm = "") {
+	console.log(whichForm);
+	if (whichForm.toLowerCase() === "edit") main.insertAdjacentHTML("beforeend", Overlay(EditForm()));
+	else main.insertAdjacentHTML("beforeend", Overlay(AddForm()));
+}
+
+function closeForm() {
+	const overlay = document.querySelector(".overlay");
+	overlay.classList.replace("fade-in", "fade-out");
+	overlay.addEventListener("animationend", () => overlay.remove());
+}
