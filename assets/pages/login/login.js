29a7c61979d76af7da78c8fb8d6b5f0a03af3f39
oumaima ait said <oumaimabellanova@gmail.com>
2024-11-14 21:39:24 +0100

    Add event listener for login form submission and prevent default action

--- assets/pages/login/login.js
+++ assets/pages/login/login.js
@@ -1,5 +1,9 @@
 const signUpLink = document.querySelector('.signUpBtn-link');
 const signInLink = document.querySelector('.signInBtn-link');
+const btnLogin = document.querySelector('.btn-Login');
+const signUpForm = document.getElementById('signUpForm');
+const loginForm = document.getElementById('loginForm');
+const inputs = document.querySelectorAll("input");
 
 if (signUpLink && signInLink) {
     signUpLink.addEventListener('click', (e) => {
@@ -12,8 +16,20 @@ if (signUpLink && signInLink) {
         e.preventDefault();
         document.querySelector('.sign-in').style.transform = 'translateY(0)';
         document.querySelector('.sign-up').style.transform = 'translateY(100%)';
-
-        
     });
 }
-       
+
+
+signUpForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const newUserName = document.getElementById('newUserName').value;
+    const newPassword = document.getElementById('newPassword').value;
+
+    if (newUserName && newPassword) {
+        localStorage.setItem("Username", newUserName);
+        localStorage.setItem("Password", newPassword);
+        alert("Inscription réussie ! Vous pouvez maintenant vous connecter.");
+        signUpForm.reset();
+    }
+});
+
