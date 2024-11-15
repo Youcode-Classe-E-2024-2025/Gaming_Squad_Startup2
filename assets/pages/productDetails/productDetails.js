6d0979c633c571796402154b6b5e75ce521dcae3
ismail baoud <ibaoud46@gmail.com>
2024-11-15 16:28:02 +0100

    modifer les images et la fonctionnalite de productDetails

--- assets/pages/productDetails/productDetails.js
+++ assets/pages/productDetails/productDetails.js
@@ -9,47 +9,43 @@ const titre = document.getElementById('titre');
 const desc = document.getElementById('desc');
 const btn = document.getElementById('btn');
 const prix = document.getElementById('prix');
-const stars = document.getElementById("stars")
 
 let products = JSON.parse(localStorage.getItem("products"));
 
-let cont = -1;
-btn.addEventListener('click', ()=>{
-   
-   cont++;
-   
-titre.textContent = `${products[cont].name}`
-desc.textContent = `${products[cont].description}`
-prix.textContent = `${products[cont].price} DH`
-img_prancipal.src = products[cont].imgSrc[0];
-for(let i = 0; i<products[cont].rating;i++){
-   stars.innerHTML = '<img class="inline w-[4vw] h-[4vw] md:w-[2vw] md:h-[2vw]" src="https://img.icons8.com/fluency/48/star--v1.png" alt=""></img>'
-}
-img_1.src = products[cont].imgSrc[0];
-img_2.src = products[cont].imgSrc[1];
-img_3.src = products[cont].imgSrc[2];
+
+plus.addEventListener('click',()=>{
+   quantity.value = Number(quantity.value) + 1;                 
+});
+moin.addEventListener('click',()=>{
+   if(quantity.value > 1){
+       quantity.value = Number(quantity.value) - 1;
+   };
+});
+
+
+
+titre.textContent = `${products[id].name}`
+desc.textContent = `${products[id].description}`
+prix.textContent = `${products[id].price} DH`
+img_prancipal.src = products[id].imgSrc[0];
+img_1.src = products[id].imgSrc[0];
+img_2.src = products[id].imgSrc[1];
+img_3.src = products[id].imgSrc[2];
  img_1.addEventListener('click', ()=>{
-    img_prancipal.src = products[cont].imgSrc[0];
+    img_prancipal.src = products[id].imgSrc[0];
  });
 
  img_2.addEventListener('click', ()=>{
-    img_prancipal.src = products[cont].imgSrc[1]; 
+    img_prancipal.src = products[id].imgSrc[1]; 
  });
  img_3.addEventListener('click', ()=>{
-    img_prancipal.src = products[cont].imgSrc[2]
+    img_prancipal.src = products[id].imgSrc[2]
  });
- plus.addEventListener('click',()=>{
-    quantity.value = Number(quantity.value) + 1;                 
+
+
+ document.body.addEventListener('click', ()=>{
+    console.log(quantity.value);
  });
- moin.addEventListener('click',()=>{
-    if(quantity.value > 1){
-        quantity.value = Number(quantity.value) - 1;
-    };
- });
-});
-//  document.body.addEventListener('click', ()=>{
-//     console.log(quantity.value);
-//  });
 
 
 
